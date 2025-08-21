b9c9dd6e271f89afe6178c629765b62d8ccc2ecc
Ken <kenppires.icloud@gmail.com>
2025-08-21 23:45:01 +0000

    📑Aula 6 -Dicionarios -CRUD> --.venv --Requirements

---
+++ aula06/dicionarios.py
@@ -0,0 +1,94 @@
+###Criar(Create)
+
+# dicionario = {
+#   "Marca": "Ford",
+#   "modelo": "Mustang",
+#   "Ano": 1964
+# }
+
+# #print(dicionario)
+
+###Acessar(Read)
+
+# dicionario = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# Modelo = dicionario["model"]
+
+# print(Modelo)
+
+##Chave
+
+# dicionario = {
+#   "Marca": "Ford",
+#   "modelo": "Mustang",
+#   "Ano": 1964
+# }
+
+# chaves = dicionario.keys()
+
+# print(chaves)
+
+
+###Mudar(Update)
+
+# dicionario = {
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# dicionario.update({"Ano": 2020})
+
+# print(dicionario)
+
+###Adicionar (Update)
+
+# dicionario = {
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# dicionario.update({"Cor": "Vermelho"})
+
+# print(dicionario)
+
+###Remover (Delete)
+
+# dicionario =	{
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# dicionario.pop("Modelo")  #apaga uma chave/item específico
+# print(dicionario)
+
+
+# dicionario = {
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# del dicionario["Modelo"]  #apaga uma chave/item específico
+# print(dicionario)
+
+
+# dicionario =	{
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# dicionario.popitem()  #apaga o último item inserido
+# print(dicionario)
+
+
+# dicionario =	{
+#   "Marca": "Ford",
+#   "Modelo": "Mustang",
+#   "Ano": 1964
+# }
+# del dicionario  #deleta todo o dicionário
+# print(dicionario)  #exibirá um erro pois o dicionário foi deletado
+
+
